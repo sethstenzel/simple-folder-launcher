e7c7ea47b5ff7e33019bbcdcd55eca5e40ecef80
Seth C. Stenzel <seth.c.stenzel@gmail.com>
2024-04-10 00:09:05 -0500

    change the default starting path location for the open file and open folder dialogues to the cwd

--- functions/files_and_folders.py
+++ functions/files_and_folders.py
@@ -1,14 +1,14 @@
 from tkinter import filedialog
+import os
+
 
 # Function to open a folder dialog and set the folder path in the folder_path_entry
 def choose_folder():
-    folder_path = filedialog.askdirectory()
+    folder_path = filedialog.askdirectory(initialdir=f"{os.getcwd()}")
     return folder_path.replace("/", "\\")
 
 
 # Function to open a file dialog and set the file path in the file_path_entry
 def choose_file():
-    file_path = filedialog.askopenfilename()
+    file_path = filedialog.askopenfilename(initialdir=f"{os.getcwd()}")
     return file_path.replace("/", "\\")
-
-
